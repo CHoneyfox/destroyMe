1439fa4c0c49c26c4f30e034e02dda2c825c95e8
CHoneyfox <alex.fuchs970@gmail.com>
2023-09-08 13:35:49 +0200

    initial commit

---
+++ interface/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class File(models.Model):
+    destroyed_by = models.CharField(max_length=20)
+    message = models.CharField(max_length=300)
+    
+    path = models.CharField(max_length=100)
+    name = models.CharField(max_length=30)
+    time_stamp = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
